28bf12b33ea4fec0458b260e93920e69134b31da
maste150hhu <mastremolten2@gmail.com>
2021-11-20 17:51:17 +0100

    extract keybindings into separate class

---
+++ public/SimpleBoxController.js
@@ -0,0 +1,37 @@
+class SimpleBoxController {
+
+    keymap = new Map()
+
+    constructor() {
+
+    }
+
+    setActionForKey(key, action = function() {console.warn(`No action set for ${key}.`)}) {
+        this.keymap.set(key, function() {return action});
+    } 
+
+    applyKeysToWindow() {
+        document.body.addEventListener("keypress", function(e) {
+            e.preventDefault();
+            if(e.key == 'w') {
+              box.position.add(new THREE.Vector3(1,0,0));
+            }
+            if(e.key == 'a') {
+              box.position.add(new THREE.Vector3(0,0,1));
+            }
+            if(e.key == 's') {
+              box.position.add(new THREE.Vector3(-1,0,0));
+            }
+            if(e.key == 'd') {
+              box.position.add(new THREE.Vector3(0,0,-1));
+            }
+            
+          });
+    }
+
+    getActionForKey(key) {
+        if([...this.keymap.keys()].includes(key))
+            return this.keymap.get(key);
+        else return null
+    }
+}
